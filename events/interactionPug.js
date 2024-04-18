65dc48050609bec19186e94e40da63602c860682
Beyonder <dalonvia@gmail.com>
2024-04-18 09:59:21 -0500

    Caracteristicas agregadas: Para el bot del competitivo se han agregado las siguientes funciones. -leer el mensaje pug -recibir las ids de los usuarios -retirar estas ids de la lista de espera -se esta implementando la funcion de actualizar embeds dinamicamente

--- events/interactionPug.js
+++ events/interactionPug.js
@@ -1,4 +1,4 @@
-const { Events, ChannelType} = require('discord.js');
+const { Events, ChannelType } = require('discord.js');
 
 // Lista de jugadores inscritos
 let players = [];
@@ -6,21 +6,26 @@ let players = [];
 module.exports = {
   name: Events.InteractionCreate,
   async execute(interaction) {
-    if (!interaction.isButton()) return;
+
+    if (!interaction.isButton()) return;//funcion para el uso del boton
 
     if (interaction.customId === 'inscribirse') {
-      // Verificar si la lista ya está llena
-      if (players.length >= 10) {
-        await interaction.reply({ content: 'La PUG ya está llena.', ephemeral: true });
-        return;
+
+      if (players.includes(interaction.user.id)) {
+        console.log('Esa madre ya existe we');
+        const repeatMessage = await interaction.reply({ content: `${interaction.user} ya te has inscrito antes, no seas tonto.`, lifetime: 10000 });//creacion de un mensaje con 10 segundos de vida
+        setTimeout(() => {
+          repeatMessage.delete();
+        }, 4000); // Eliminar el mensaje después de 4 segundos\
+
+      } else {
+        players.push(interaction.user.id);
+        console.log('Agregado papu');
+        const successMessage = await interaction.reply({ content: `${interaction.user} te has inscrito correctamente, ¡bien hecho!`, lifetime: 10000 });
+        setTimeout(() => {
+          successMessage.delete();
+        }, 4000); // Eliminar el mensaje después de 4 segundos
       }
-
-      // Agregar jugador a la lista
-      players.push(interaction.user.tag);
-
-      // Actualizar mensaje con la lista de jugadores
-      await interaction.channel.send(`[${players.length} de 10] ${interaction.user.tag} se ha inscrito a la PUG. Faltan ${10 - players.length} jugadores.`);
-
       // Verificar si la lista está completa
       if (players.length === 10) {
         // Mensaje de inicio de la PUG
@@ -29,7 +34,7 @@ module.exports = {
         // Crear canal de voz
         const category = interaction.guild.channels.cache.find(c => c.type === ChannelType.GuildCategory && c.id === '1219775758248181840');
         const voiceChannel = await interaction.guild.channels.create({
-          name:'PUG Voice Channel',
+          name: 'PUG Voice Channel',
           type: ChannelType.GuildVoice,
           parent: category.id,
           permissionOverwrites: [
@@ -44,18 +49,39 @@ module.exports = {
           ],
         });
 
-         // Mover a los jugadores al canal de voz y enviar mensaje directo si no están conectados
-         // TODO: hay un bug donde el for manda 10 veces el mensaje preparacion al usuario 
+        // Mover a los jugadores al canal de voz y enviar mensaje directo si no están conectados
+        // TODO: hay un bug donde el for manda 10 veces el mensaje preparacion al usuario 
 
-         for (const player of players) {
-            const member = interaction.guild.members.cache.find(m => m.user.tag === player);
-            if (member && member.voice.channel) {
-              await member.voice.setChannel(voiceChannel);
-            } else {
-              await interaction.user.send('La PUG ha comenzado. Únete al canal de voz para participar.');
-            }
+        for (const player of players) {
+          const member = interaction.guild.members.cache.find(m => m.user.id === player);
+          if (member && member.voice.channel) {
+            await member.voice.setChannel(voiceChannel);
+          } else {
+            await interaction.user.send('La PUG ha comenzado. Únete al canal de voz para participar.');
           }
+        }
       }
     }
+
+    if (interaction.customId === 'retirarse') {
+
+      if (players.length === 0) {
+        const otromensaje = await interaction.reply({ content: `${interaction.user} no puedes retirarte, no te has inscrito.` });
+        setTimeout(() => {
+          otromensaje.delete();
+        }, 4000); //
+      }
+
+      else {
+        players = players.filter(id => id !== interaction.user.id);
+        console.log('Usuario retirado correctamente.');
+        const withdrawMessage = await interaction.reply({ content: `${interaction.user} te has retirado correctamente.` });
+        setTimeout(() => {
+          withdrawMessage.delete();
+        }, 4000); // Eliminar el mensaje después de 4 segundos
+      }
+
+    }
+
   }
 };
--- events/messagePug.js
+++ events/messagePug.js
@@ -12,27 +12,49 @@ module.exports = {
     }
 
     // Verificar si el mensaje es el comando !pug
-    if (message.content.toLowerCase() === '!pug') {
+    if (message.content.toLowerCase() === 'pug') {
+
       // Verificar si la lista ya está llena
-      if (players.length >= 10) {
+      if (players.length === 10) {
         message.channel.send('La PUG ya está llena.');
         return;
       }
+      //creacion del boton de inscripcion
+      //const embed = new EmbedBuilder()
+      //.setTitle('Se busca PUG')
+      //.setDescription('¡Únete a la PUG haciendo clic en el botón "Inscribirse"!')
+      //.setColor('#0099ff');
 
-      const embed = new EmbedBuilder()
+      const embed1 = new EmbedBuilder()
         .setTitle('Se busca PUG')
         .setDescription('¡Únete a la PUG haciendo clic en el botón "Inscribirse"!')
-        .setColor('#0099ff');
+        .setColor('Blue');
+
+      const embed2 = new EmbedBuilder()
+        .setTitle('lista: ')
+        .setDescription(players.length > 0 ? players.join('\n') : 'No hay jugadores inscritos aún.')
+        .setColor('Red');
 
       const row = new ActionRowBuilder()
         .addComponents(
           new ButtonBuilder()
             .setCustomId('inscribirse')
             .setLabel('Inscribirse')
-            .setStyle('Primary')
-        );
+            .setStyle('Primary'),
 
-      await message.channel.send({ embeds: [embed], components: [row] });
+          new ButtonBuilder()
+            .setCustomId('retirarse')
+            .setLabel('Retirarse')
+            .setStyle('Danger')
+
+        );
+      // Enviar primer embed y acción
+      const message1 = await message.channel.send({ embeds: [embed1], components: [row] });
+
+      // Enviar segundo embed y acción
+      const message2 = await message.channel.send({ embeds: [embed2] });
+
+      
     }
   }
 };
