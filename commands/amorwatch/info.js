fa6d5ee5e558dc912b62d125c17fe19295697570
Sagitario-lab <ignacioulman@hotmail.com>
2024-04-22 14:32:58 -0300

    amorwatch info changes

--- commands/amorwatch/info.js
+++ commands/amorwatch/info.js
@@ -21,16 +21,24 @@ module.exports = {
                 },
                 {
                     name: '/buscarpareja',
-                    value: 'Usa este comando para abrir el menú de búsqueda de parejas'
+                    value: 'Usa este comando para abrir el menú de búsqueda de parejas.'
                 },
                 {
                     name: 'Cosas a tener en cuenta',
-                    value: '* Solo podrás buscar a una persona por día'
+                    value: '* Solo podrás buscar a una persona por día.'
                 },
                 {
                     name: '\u200B',
-                    value: '* Al crear tu cuenta se te otorgará un rol específico como participante en amorwatch'
-                }
+                    value: '* Al crear tu cuenta comprueba que todos los datos sean correctos ya que no todos se pueden editar una vez creado el perfil.'
+                },
+                {
+                    name: '\u200B',
+                    value: '* Dado que los chats son absolutamente privados entre los 2 usuarios, no nos hacemos responsables de lo que pueda pasar dentro de ellos (Aunque siempre puedes abrir un ticket!)'
+                },
+                {
+                    name: '\u200B',
+                    value: '* Cualquiera de los 2 usuarios puede terminar con la cita'
+                },
             ).setImage('https://bnetcmsus-a.akamaihd.net/cms/page_media/8e/8E77P32J7NLQ1675992201986.png')
 
         await interaction.reply({
