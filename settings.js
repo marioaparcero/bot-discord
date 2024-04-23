7673685482646ba56bceb2260e006425fc14b878
Sagitario-lab <ignacioulman@hotmail.com>
2024-04-23 00:18:56 -0300

    amorwatch settings

---
+++ settings.js
@@ -0,0 +1,13 @@
+module.exports = {
+    owner: "649094110300602408",
+    guildId: "620450878368907274",
+    canalLogEntradas: "1142514797838090341",
+    canalLogInvitacionesID: '1210808019185836082',
+    canalLogEstados: '1210808019185836082',
+    canalLogRoles: '1146188078973726730',
+    dbURL: 'mongodb+srv://amorwatch:amorwatch@amorwatch.p9dtveu.mongodb.net/',
+    colUsers: 'users',
+    colMatchs: 'matchs',
+    db: 'amorwatch',
+    parendID: '1227928553203568670'
+};
