bab043a42500492bb1ae252fdda7c949d1359271
Pablo71133 <juanpablofal2005@gmail.com>
2026-04-20 19:41:54 +0100

    Agregar comando 'chiste' para contar chistes de Overwatch al azar

---
+++ commands/fun/chiste.js
@@ -0,0 +1,19 @@
+// Hazme un comando de chiste para overwatch randomes
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('chiste')
+        .setDescription('Te cuenta un chiste de Overwatch al azar.'),
+    async execute(interaction) {
+        const chistes = [
+            '¿Por qué Tracer no puede usar el microondas? Porque siempre está en movimiento.',
+            '¿Qué le dijo Reaper a su novia? "Eres mi alma gemela".',
+            '¿Por qué Winston no puede jugar al escondite? Porque siempre se encuentra a sí mismo.',
+            '¿Qué le dijo Genji a su hermano? "Eres un ninja de pacotilla".',
+            '¿Por qué Mei no puede jugar al fútbol? Porque siempre se congela en el campo.',
+        ];
+        const chisteRandom = chistes[Math.floor(Math.random() * chistes.length)];
+        await interaction.reply(chisteRandom);
+    }
+};
